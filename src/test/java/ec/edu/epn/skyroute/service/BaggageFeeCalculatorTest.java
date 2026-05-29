c1f8e6bb6239e0362cd53ebb08acc95eae61aad0
Luis Guerrero <luis.guerrero02@epn.edu.ec>
2026-05-29 17:18:03 -0500

    preparación con before each, y configurar un void setup

---
+++ src/test/java/ec/edu/epn/skyroute/service/BaggageFeeCalculatorTest.java
@@ -0,0 +1,35 @@
+package ec.edu.epn.skyroute.service;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+
+public class BaggageFeeCalculatorTest {
+
+    //primero ponemos, como dice la lógica de negocio, el mock, el inject mocks 
+
+    @Mock
+    private PassengerService passengerService;
+
+    @InjectMocks
+    private BaggageFeeCalculator calculator;
+
+    private Long regularPassengerId;
+    private Long vipPassengerId;
+
+    @BeforeEach
+    void setUp() {
+        regularPassengerId = 55L;
+        vipPassengerId = 10L;
+    }
+    
+}
